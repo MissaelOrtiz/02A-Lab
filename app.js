1b200073539f460f3f97c8ee93b297a619d1bb59
Missael Ortiz <ortiz.missael80@gmail.com>
2021-04-28 15:37:05 -0700

    actually did the assignment

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const colorLightGreen = document.getElementById('lightGreen')
 const colorLightBlue = document.getElementById('lightBlue')
 const colorWhite = document.getElementById('white')
 const backgroundColor = document.getElementById('bg')
+const boxColor = document.getElementById('box')
 
 changeNameButton.addEventListener('click', ()=> {
     // console.log(inputName.value);
@@ -15,19 +16,23 @@ changeNameButton.addEventListener('click', ()=> {
 colorPink.addEventListener('click', ()=> {
     // console.log('I AM HERE');
     backgroundColor.style.backgroundColor = "pink";
+    boxColor.style.backgroundColor = "hotPink";
 })
 
 colorLightGreen.addEventListener('click', ()=> {
     // console.log('I AM HERE');
     backgroundColor.style.backgroundColor = "lightGreen";
+    boxColor.style.backgroundColor = "green";
 })
 
 colorLightBlue.addEventListener('click', ()=> {
     // console.log('I AM HERE');
     backgroundColor.style.backgroundColor = "lightBlue";
+    boxColor.style.backgroundColor = "blue";
 })
 
 colorWhite.addEventListener('click', ()=> {
     // console.log('I AM HERE');
     backgroundColor.style.backgroundColor = "white";
+    boxColor.style.backgroundColor = "red";
 })
